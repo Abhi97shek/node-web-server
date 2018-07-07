78eee696db7ffc476d82e948b60380f593811762
Abhi97shek <abhi.kumar736@gmail.com>
2018-07-07 23:49:06 +0530

    Initial Commit

---
+++ servers.js
@@ -0,0 +1,51 @@
+const express=require('express');
+const fs=require('fs');
+const hbs=require('hbs');
+var app=express();
+
+hbs.registerPartials(__dirname +"/views/partials");
+app.set('view engine','hbs');
+app.use(express.static(__dirname + "/public"));
+
+
+app.use((req,res,next)=>{
+var date =new Date().toString();
+var log=`${date}  ${req.method} ${req.url} ${req.hostname} ${req.ip}`;
+fs.appendFileSync('Server.log',log+"\n");
+next();
+});
+
+
+app.use((req,res,next)=>{
+
+      res.render('maintaince.hbs');
+});
+app.get('/',(req,res)=>
+{
+  res.render('home.hbs',{
+    pageTitle:"Home Page",
+    CurrentYear:new Date().getFullYear()
+  });
+
+});
+app.get('/about',(req,res)=>{
+
+      res.render('about.hbs',{
+          pageTitle:"About Page",
+          CurrentYear: new Date().getFullYear()
+
+      });
+});
+
+app.get('/bad',(req,res)=>{
+
+      res.send({
+        errormessage:"Ubale to connect to the Page",
+      });
+
+});
+
+app.listen(3000,()=>
+{
+  console.log("Server is Up on Port 3000");
+});
